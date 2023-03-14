434f2f452f21047368feeb12d43286371a26e337
JerryYin777 <943321359@qq.com>
2023-03-14 20:25:42 +0800

    Initial

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_TITLE = `Creallatie's Blog`;
+export const SITE_DESCRIPTION = 'Creallatie Site Description';
+export const SITE_EMAIL = 'congruiyin@email.ncu.edu.cn'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
