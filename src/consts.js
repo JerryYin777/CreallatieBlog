f5b8850a5b843bfcdb76010b8e01e409b871a1b1
JerryYin777 <943321359@qq.com>
2023-03-14 21:12:15 +0800

    第三次提交

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Creallatie's Blog`;
 export const SITE_DESCRIPTION = 'Creallatie Site Description';
 export const SITE_EMAIL = 'congruiyin@email.ncu.edu.cn'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_NAME = 'creallatie.icu';
+export const SITE_URL = "http://www.creallatie.icu/";
